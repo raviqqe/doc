8b269fe7e663badf032aa20de85ebec4f859e721
Yota Toyama <raviqqe@gmail.com>
2022-03-20 02:42:41 +0000

    Update serve script

--- serve.sh
+++ serve.sh
@@ -2,24 +2,4 @@
 
 set -e
 
-if [ $# -ne 1 ]; then
-  exit 1
-fi
-
-file=$1
-
-kill_all() {
-  echo $pids
-  kill $pids
-}
-
-trap kill_all SIGTERM
-
-marp -w "$file" &
-pids=$1
-
-(
-  cd $(dirname "$file")
-  python3 -m http.server
-  pids="$pids $!"
-)
+npx marp -s -w .
