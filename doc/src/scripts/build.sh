939532d6bdd84ab7bcdcb8b86cf4b8d4e82e264d
Yota Toyama <raviqqe@gmail.com>
2023-09-08 23:10:41 +1000

    Fix

--- doc/src/scripts/build.sh
+++ doc/src/scripts/build.sh
@@ -2,11 +2,10 @@
 
 set -ex
 
-directory=$(dirname $0)/..
+directory=$(dirname $0)/../..
 
-npx @marp-team/marp-cli --html --input-dir $directory/../.. --output $directory/pages
+npx @marp-team/marp-cli --html --input-dir $directory/.. --output $directory/public
 
-rm -r $directory/pages/doc
-rm $directory/pages/README.html
+rm -r $directory/public/doc
 
 npx astro build
