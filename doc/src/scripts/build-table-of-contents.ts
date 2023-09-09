3262ec3c198e7e2003eb3e7f521cb5ebf088296e
Yota Toyama <raviqqe@gmail.com>
2023-09-09 16:40:07 +0930

    Fix post pages (#58)
    * Move slide files
    
    * Move
    
    * Move files
    
    * Remove date
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix dates
    
    * Fix spell check

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -4,31 +4,35 @@ import { exec } from "node:child_process";
 import { promisify } from "node:util";
 import { sortBy } from "lodash-unified";
 
-await writeFile(
-  "src/components/TableOfContents.md",
-  sortBy(
-    await Promise.all(
-      (await glob("../**/*.md", { ignore: ["node_modules/**", "src/**"] }))
-        .filter((path) => !path.includes("README"))
-        .map(async (path) => {
-          const title = (await readFile(path, "utf-8"))
-            .split("\n")[0]
-            .replace("# ", "");
-          const htmlPath = path.replace(/^..\//, "").replace(".md", ".html");
-          const time = (
-            await promisify(exec)(
-              `git log --format=format:%ci --name-only --diff-filter=A ${path}`,
-            )
-          ).stdout
-            .split(" ")[0]
-            .replaceAll("-", "/");
+const writeToc = async (directory: string, component: string) =>
+  await writeFile(
+    `src/components/${component}.md`,
+    sortBy(
+      await Promise.all(
+        (await glob(`../${directory}/**/*.md`))
+          .filter((path) => !path.includes("README"))
+          .map(async (path) => {
+            const title = (await readFile(path, "utf-8"))
+              .split("\n")[0]
+              .replace("# ", "");
+            const htmlPath = path.replace(/^..\//, "").replace(".md", ".html");
+            const time = (
+              await promisify(exec)(
+                `git log --format=format:%ci --follow --name-only --diff-filter=A ${path}`,
+              )
+            ).stdout
+              .split(" ")[0]
+              .replaceAll("-", "/");
 
-          return { title, path: htmlPath, time };
-        }),
-    ),
-    "time",
-  )
-    .reverse()
-    .map(({ title, path, time }) => `- [${title}](${path}) (${time})`)
-    .join("\n"),
-);
+            return { title, path: htmlPath, time };
+          }),
+      ),
+      "time",
+    )
+      .reverse()
+      .map(({ title, path, time }) => `- [${title}](${path}) (${time})`)
+      .join("\n"),
+  );
+
+await writeToc("posts", "Posts");
+await writeToc("slides", "Slides");
