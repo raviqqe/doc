9fe670d31ea3fe36eb8bcb871f40635b66974e95
Yota Toyama <raviqqe@gmail.com>
2023-09-09 16:26:42 +1000

    Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -1,5 +1,7 @@
 import { glob } from "glob";
 import { readFile, writeFile } from "node:fs/promises";
+import { exec } from "node:child_process";
+import { promisify } from "node:util";
 
 await writeFile(
   "src/components/TableOfContents.md",
@@ -11,9 +13,16 @@ await writeFile(
           const title = (await readFile(path, "utf-8"))
             .split("\n")[0]
             .replace("# ", "");
-          path = path.replace(/^..\//, "").replace(".md", ".html");
+          const htmlPath = path.replace(/^..\//, "").replace(".md", ".html");
+          const time = (
+            await promisify(exec)(
+              `git log --format=format:%ci --name-only --diff-filter=A ${path}`,
+            )
+          ).stdout
+            .split(" ")[0]
+            .replaceAll("-", "/");
 
-          return `- [${title}](${path})`;
+          return `- [${title}](${htmlPath}) (${time})`;
         }),
     )
   ).join("\n"),
