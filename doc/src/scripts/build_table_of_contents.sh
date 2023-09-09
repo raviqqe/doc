fde658ad08deda43916df6dabc90b22797837c40
Yota Toyama <raviqqe@gmail.com>
2023-09-09 11:32:54 +1000

    Fix

--- doc/src/scripts/build_table_of_contents.sh
+++ doc/src/scripts/build_table_of_contents.sh
@@ -4,9 +4,11 @@ set -e
 
 cd $(dirname $0)/../../..
 
+mkdir -p doc/src/components
+
 for file in $(find . -name '*.md' | sort | grep -iv -e node_modules -e readme -e doc); do
   path=$(echo $file | sed 's/\.md/\.html/g' | sed 's/^\.\///')
   title=$(cat $file | grep '^# .*$' | head -n 1 | sed 's/# *//g')
 
   echo "- [$title]($path)"
-done
+done >doc/src/components/TableOfContents.md
