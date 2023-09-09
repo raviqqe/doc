09787e4fae3354fc7b789ba3f425caaf63507927
Yota Toyama <raviqqe@gmail.com>
2023-09-09 16:17:35 +1000

    Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -5,7 +5,7 @@ await writeFile(
   "src/components/TableOfContents.md",
   (
     await Promise.all(
-      (await glob("../**/*.md", { ignore: ["node_modules/**", "**/src/**"] }))
+      (await glob("../**/*.md", { ignore: ["node_modules/**", "src/**"] }))
         .filter((path) => !path.includes("README"))
         .map(async (path) => {
           const title = (await readFile(path, "utf-8"))
