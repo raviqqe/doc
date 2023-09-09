1ef68469276f70aea4f55ffd36ccf9526d3ee270
Yota Toyama <raviqqe@gmail.com>
2023-09-09 11:21:46 +0930

    Fix build (#54)
    Fix

--- doc/src/scripts/build.sh
+++ doc/src/scripts/build.sh
@@ -6,7 +6,7 @@ directory=$(dirname $0)/../..
 
 npx @marp-team/marp-cli --html --input-dir $directory/.. --output $directory/public
 
-rm -r $directory/public/doc
+rm -rf $directory/public/doc
 
 npm run build:toc
 npx astro build
