e4cb07244353c4fdce150de86e171d0fe6cc1d4f
Yota Toyama <raviqqe@gmail.com>
2023-09-09 17:19:13 +0930

    Layout (#60)
    * Fix
    
    * Fix
    
    * fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

---
+++ doc/src/scripts/build-posts.ts
@@ -0,0 +1,24 @@
+import { glob } from "glob";
+import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { dirname } from "node:path";
+
+await Promise.all(
+  (await glob(`../posts/**/*.md`)).map(async (path) => {
+    const content = await readFile(path, "utf-8");
+    const title = content.split("\n")[0].replace("# ", "");
+    path = path.replace("../", "src/pages/");
+
+    await mkdir(dirname(path), { recursive: true });
+    await writeFile(
+      path,
+      [
+        "---",
+        "layout: ../../../layouts/Default.astro",
+        `title: ${title}`,
+        "---",
+        "",
+        content,
+      ].join("\n"),
+    );
+  }),
+);
