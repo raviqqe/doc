6682542a22772cca3cfa6ee0455f71936fdece9a
Yota Toyama <raviqqe@gmail.com>
2023-09-09 16:15:23 +1000

    Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -5,17 +5,13 @@ await writeFile(
   "src/components/TableOfContents.md",
   (
     await Promise.all(
-      (
-        await glob("**/*.md", {
-          ignore: ["node_modules/**", "doc/**"],
-          root: "..",
-        })
-      )
-        .filter((path) => !path.includes("/doc/"))
+      (await glob("../**/*.md", { ignore: ["node_modules/**", "**/src/**"] }))
+        .filter((path) => !path.includes("README"))
         .map(async (path) => {
           const title = (await readFile(path, "utf-8"))
             .split("\n")[0]
             .replace("# ", "");
+          path = path.replace(/^..\//, "").replace(".md", ".html");
 
           return `- [${title}](${path})`;
         }),
