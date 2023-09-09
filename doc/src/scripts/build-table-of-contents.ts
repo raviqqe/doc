0eb302e3308f4ade4aaae060e93e045f38f40230
Yota Toyama <raviqqe@gmail.com>
2023-09-09 16:03:05 +1000

    Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -1,7 +1,7 @@
 import { glob } from "glob";
 
 console.log(
-  (await glob("../**/*.md", { ignore: ["node_modules", "doc"] })).filter(
+  (await glob("../**/*.md", { ignore: ["node_modules/**", "doc/**"] })).filter(
     (path) => !path.includes("/doc/"),
   ),
 );
