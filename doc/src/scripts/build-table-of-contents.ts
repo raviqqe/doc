c248618885fe5222f0c565bac245b120c26c1eaa
Yota Toyama <raviqqe@gmail.com>
2023-10-01 19:23:09 +1100

    Year section (#104)
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -2,45 +2,54 @@ import { glob } from "glob";
 import { readFile, stat, writeFile } from "node:fs/promises";
 import { exec } from "node:child_process";
 import { promisify } from "node:util";
-import { sortBy } from "lodash-unified";
+import lodash from "lodash";
 import { join } from "node:path";
 
 const writeToc = async (directory: string, component: string) =>
   await writeFile(
     `src/components/${component}.md`,
-    sortBy(
-      await Promise.all(
-        (await glob(`../${directory}/**/*.md`)).map(async (path) => {
-          const htmlPath = path.replace(/^..\//, "").replace(".md", "");
-          const pdfPath = htmlPath + ".pdf";
+    lodash
+      .chain(
+        await Promise.all(
+          (await glob(`../${directory}/**/*.md`)).map(async (path) => {
+            const htmlPath = path.replace(/^..\//, "").replace(".md", "");
+            const pdfPath = htmlPath + ".pdf";
 
-          return {
-            title: (await readFile(path, "utf-8"))
-              .split("\n")[0]
-              .replace("# ", ""),
-            htmlPath,
-            pdfPath: (await stat(join("public", pdfPath)).catch(() => null))
-              ? pdfPath
-              : null,
-            time: (
-              await promisify(exec)(
-                `git log --format=format:%ci --follow --name-only --diff-filter=A ${path}`,
-              )
-            ).stdout
-              .split(" ")[0]
-              .replaceAll("-", "/"),
-          };
-        }),
-      ),
-      "time",
-    )
-      .reverse()
-      .map(
-        ({ title, htmlPath, pdfPath, time }) =>
-          `- [${title}](${htmlPath}) (${
-            pdfPath ? `[PDF](${pdfPath}), ` : ""
-          }${time})`,
+            return {
+              title: (await readFile(path, "utf-8"))
+                .split("\n")[0]
+                .replace("# ", ""),
+              htmlPath,
+              pdfPath: (await stat(join("public", pdfPath)).catch(() => null))
+                ? pdfPath
+                : null,
+              time: (
+                await promisify(exec)(
+                  `git log --format=format:%ci --follow --name-only --diff-filter=A ${path}`,
+                )
+              ).stdout
+                .split(" ")[0]
+                .replaceAll("-", "/"),
+            };
+          }),
+        ),
       )
+      .sortBy("time")
+      .reverse()
+      .groupBy(({ time }) => Number(time.split("/")[0]))
+      .toPairsIn()
+      .sort()
+      .reverse()
+      .flatMap(([year, posts]) => [
+        `### ${year}`,
+        ...posts.map(
+          ({ title, htmlPath, pdfPath, time }) =>
+            `- [${title}](${htmlPath}) (${
+              pdfPath ? `[PDF](${pdfPath}), ` : ""
+            }${time})`,
+        ),
+      ])
+      .value()
       .join("\n"),
   );
 
