8b7bfd3f5b0ca05b42448b91e5dc1db74ff5f7a8
Yota Toyama <raviqqe@gmail.com>
2023-10-17 15:05:17 +1100

    Fix

--- doc/src/scripts/build-posts.ts
+++ doc/src/scripts/build-posts.ts
@@ -5,7 +5,6 @@ import { dirname } from "node:path";
 await Promise.all(
   (await glob("../posts/**/*.md")).map(async (path) => {
     const content = await readFile(path, "utf-8");
-    const title = content.split("\n")[0].replace("# ", "");
     path = path.replace("../", "src/pages/");
 
     await mkdir(dirname(path), { recursive: true });
@@ -14,7 +13,7 @@ await Promise.all(
       [
         "---",
         "layout: ../../../layouts/Default.astro",
-        `title: ${title}`,
+        `title: ${content.split("\n")[0].replace("# ", "")}`,
         "---",
         "",
         content,
