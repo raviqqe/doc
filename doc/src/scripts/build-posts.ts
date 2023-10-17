6f637d1a8b4055ab3173bd02e3cbdc2ef57181da
Yota Toyama <raviqqe@gmail.com>
2023-10-17 15:04:16 +1100

    Fix

--- doc/src/scripts/build-posts.ts
+++ doc/src/scripts/build-posts.ts
@@ -3,7 +3,7 @@ import { mkdir, readFile, writeFile } from "node:fs/promises";
 import { dirname } from "node:path";
 
 await Promise.all(
-  (await glob(`../posts/**/*.md`)).map(async (path) => {
+  (await glob("../posts/**/*.md")).map(async (path) => {
     const content = await readFile(path, "utf-8");
     const title = content.split("\n")[0].replace("# ", "");
     path = path.replace("../", "src/pages/");
