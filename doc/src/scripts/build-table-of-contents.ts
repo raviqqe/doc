f36f2e84ca24331c60addb821e95d3f3c99a1bb6
Yota Toyama <raviqqe@gmail.com>
2023-12-10 13:37:17 +0900

    Reverse ToC (#226)
    * Reverse ToC
    
    * Fix

--- doc/src/scripts/build-table-of-contents.ts
+++ doc/src/scripts/build-table-of-contents.ts
@@ -47,12 +47,14 @@ const writeToc = async (directory: string, component: string) =>
       .reverse()
       .flatMap(([year, posts]) => [
         `### ${year}`,
-        ...posts.map(
-          ({ title, htmlPath, pdfPath, time }) =>
-            `- [${title}](${htmlPath}) (${
-              pdfPath ? `[PDF](${pdfPath}), ` : ""
-            }${time})`,
-        ),
+        ...posts
+          .reverse()
+          .map(
+            ({ title, htmlPath, pdfPath, time }) =>
+              `- [${title}](${htmlPath}) (${
+                pdfPath ? `[PDF](${pdfPath}), ` : ""
+              }${time})`,
+          ),
       ])
       .join("\n"),
   );
