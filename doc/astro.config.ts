7c3c0481c142237a02114b8b70a1336fd577b30c
Yota Toyama <raviqqe@gmail.com>
2024-12-24 17:58:58 +0900

    Fix

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -4,6 +4,7 @@ import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkMermaid from "remark-mermaidjs";
+import rehypeAutoLinkHeadings from "rehype-autolink-headings";
 
 export default defineConfig({
   base: "/doc",
@@ -18,6 +19,7 @@ export default defineConfig({
   ],
   markdown: {
     remarkPlugins: [remarkMermaid, remarkToc],
+    rehypePlugins: [rehypeAutoLinkHeadings()],
   },
   site: "https://raviqqe.github.io/doc",
 });
