20849dd4f3f5125a9bc3f3a5a9cc0449d53ddcc8
Yota Toyama <raviqqe@gmail.com>
2025-04-21 10:07:02 +0900

    Update document (#792)
    - **Update document**
    - **Fix**

--- doc/astro.config.ts
+++ doc/astro.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import rehypeMermaid from "rehype-mermaid";
 import remarkToc from "remark-toc";
-import remarkMermaid from "remark-mermaidjs";
 import rehypeSlug from "rehype-slug";
 import rehypeAutoLinkHeadings from "rehype-autolink-headings";
 
@@ -22,8 +22,22 @@ export default defineConfig({
     sitemap(),
   ],
   markdown: {
-    remarkPlugins: [remarkMermaid, remarkToc],
-    rehypePlugins: [rehypeSlug, rehypeAutoLinkHeadings],
+    syntaxHighlight: {
+      type: "shiki",
+      excludeLangs: ["mermaid"],
+    },
+    remarkPlugins: [remarkToc],
+    rehypePlugins: [
+      [
+        rehypeMermaid,
+        {
+          dark: true,
+          strategy: "img-svg",
+        },
+      ],
+      rehypeSlug,
+      rehypeAutoLinkHeadings,
+    ],
   },
   site: "https://raviqqe.github.io/doc",
 });
