b9b0b11747813302b8d9df3d92a79c69447631f2
Yota Toyama <raviqqe@gmail.com>
2025-06-17 10:18:37 +0800

    Public PDF papers (#849)

--- build.sh
+++ build.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-set -e
+set -ex
+
+directory=doc/public/papers
+mkdir -p $directory
 
 for file in $(find . -name '*.tex'); do
   (
@@ -15,4 +18,7 @@ for file in $(find . -name '*.tex'); do
     lualatex --halt-on-error $file
     lualatex --halt-on-error $file
   )
+
+  file=${file%.tex}
+  cp $file.pdf $directory/$(basename $(dirname $file)).pdf
 done
