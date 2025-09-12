ddd2bbbb671919bad5a0f69d7cee70c5ec2c4356
Yota Toyama <raviqqe@gmail.com>
2025-09-11 20:16:42 -0700

    Fix

--- tools/build_mermaid.sh
+++ tools/build_mermaid.sh
@@ -3,5 +3,5 @@
 set -ex
 
 for file in $(find . -name '*.mmd'); do
-  mmdc --puppeteerConfigFile puppeteer.json --width 1600 --height 900 -i $file -o ${file%.mmd}.svg
+  mmdc --puppeteerConfigFile puppeteer.json --width 1600 --height 900 --theme forest -i $file -o ${file%.mmd}.svg
 done
