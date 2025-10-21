2f9a07cf3d3a49df8760018bfe61805bd9df9bfc
Yota Toyama <raviqqe@gmail.com>
2025-10-21 18:34:05 +0900

    Fix

--- doc/src/scripts/build-directory.ts
+++ doc/src/scripts/build-directory.ts
@@ -1,26 +1,27 @@
-import { mkdir, readFile, writeFile } from "node:fs/promises";
+import { glob, mkdir, readFile, writeFile } from "node:fs/promises";
 import { dirname, join, relative } from "node:path";
 import { argv } from "node:process";
-import { glob } from "glob";
 
 const [, , directory] = argv;
 
 await Promise.all(
-  (await glob(`../${directory}/**/*.md`)).map(async (path) => {
-    const content = await readFile(path, "utf-8");
-    path = join("src/pages", relative("..", path));
+  (await Array.fromAsync(glob(join("..", directory, "**/*.md")))).map(
+    async (path) => {
+      const content = await readFile(path, "utf-8");
+      path = join("src/pages", relative("..", path));
 
-    await mkdir(dirname(path), { recursive: true });
-    await writeFile(
-      path,
-      [
-        "---",
-        `layout: ${relative(dirname(path), "src/layouts/Default.astro")}`,
-        `title: ${JSON.stringify(content.split("\n")[0].replace("# ", ""))}`,
-        "---",
-        "",
-        content,
-      ].join("\n"),
-    );
-  }),
+      await mkdir(dirname(path), { recursive: true });
+      await writeFile(
+        path,
+        [
+          "---",
+          `layout: ${relative(dirname(path), "src/layouts/Default.astro")}`,
+          `title: ${JSON.stringify(content.split("\n")[0].replace("# ", ""))}`,
+          "---",
+          "",
+          content,
+        ].join("\n"),
+      );
+    },
+  ),
 );
