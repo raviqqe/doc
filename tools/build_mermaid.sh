6a2ef4b252c3ae243dcffe46cedf1c81b3cf48cf
Yota Toyama <raviqqe@gmail.com>
2026-09-04 19:37:26 -0700

    Build Mermaid files in parallel

--- tools/build_mermaid.sh
+++ tools/build_mermaid.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-set -ex
+set -e
 
 for file in $(find . -name '*.mmd'); do
-  mmdc \
+  echo -i $file -o ${file%.mmd}.svg
+done |
+  xargs -L 1 -P 0 \
+    mmdc \
     --backgroundColor transparent \
     --configFile mermaid.json \
     --cssFile mermaid.css \
     --puppeteerConfigFile puppeteer.json \
-    --theme dark \
-    -i $file -o ${file%.mmd}.svg
-done
+    --theme dark
